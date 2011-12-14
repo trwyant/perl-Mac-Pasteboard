077bf4d700727ffae4711cf9806b781a6f8ec463
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2011-12-14 16:27:56 +0000

    Update version to 0.003. Update Changes file for changes (none) since 0.002_91.
    git-svn-id: file:///Users/tom/Code/svn/repos/Mac-Pasteboard/trunk@2778 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Mac/Pasteboard.pm
+++ lib/Mac/Pasteboard.pm
@@ -43,7 +43,7 @@ our @ISA = qw(Exporter);
     our @EXPORT = @funcs;	## no critic (ProhibitAutomaticExportation)
 }
 
-our $VERSION = '0.002_91';
+our $VERSION = '0.003';
 our $XS_VERSION = $VERSION;
 our $ALPHA_VERSION = $VERSION;
 $VERSION =~ s/_//g;
